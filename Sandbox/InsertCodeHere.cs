6f89a42d3af911914293559508c602d073c0514c
Poul Henriksen <pohe@easj.dk>
2019-09-12 13:12:46 +0200

    Add project files.

---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Sandbox
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Student s1 = new Student("Victor"); // creates an object of a Student class
+            //s1.Name = "Victor"
+
+            Student s2 = new Student();
+            Console.WriteLine($"S2s navn {s2.Name}");
+            s2.Name = "Charlotte";
+            Console.WriteLine($"S2s navn {s2.Name}");
+
+            Console.WriteLine($"S1s name {s1.Name}");
+
+            Student s3 = new Student("Magnus");
+            s3.Grade1 = -500;
+            s3.Grade2 = 700;
+            double gennemsnit= s3.CalculateAverage();
+            Console.WriteLine($"Gennemsnit for s3 {gennemsnit}");
+            s1.Cpr = 0310395453; 
+            s2.Cpr = 0302944444;
+
+            //Grade 2
+            s1.Grade2 = 12;
+            s2.Grade2 = 7;
+            s1.Mobile = "0045-66666"; 
+            Console.WriteLine($"S1s cpr {s1.Cpr} og mobile nummer {s1.Mobile}");
+            Console.WriteLine($"S2s name {s2.Cpr}");
+
+            Student s4 = new Student("Poul", "111",6666);
+           
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ Sandbox/Student.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    // A class i s a template
+    public class Student
+    {
+        //instance field
+
+        private string _name;
+        private int _cpr;
+        private int _grade1;
+        private string _mblNr;
+        private int _grade2;
+        
+        //property
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int Cpr
+        {
+            get
+            {
+                return _cpr;
+
+            }
+            set
+            {
+                _cpr = value;
+
+            }
+        }
+
+        public int Grade1
+        {
+            get
+            {
+                return _grade1;
+            }
+            set
+            {
+                _grade1 = value;
+            }
+        }
+
+        public int Grade2
+        {
+            get
+            {
+                return _grade2;
+            }
+            set
+            {
+                _grade2 = value;
+            }
+        }
+
+       
+
+        public string Mobile
+        {
+            get
+            {
+                return _mblNr;
+            }
+
+            set
+            {
+                _mblNr = value;
+            }
+        }
+
+        public Student(string name)
+        {
+            _name = name;
+        }
+
+        public Student(string name, string mobile, int cpr)
+        {
+            _name = name;
+            _mblNr = mobile;
+            _cpr = cpr; 
+        }
+        public Student()
+        {
+
+        }
+
+        public double CalculateAverage()
+        {
+            double result = (_grade1 + _grade2) / 2.0;
+            return result; 
+        }
+    }
+}
